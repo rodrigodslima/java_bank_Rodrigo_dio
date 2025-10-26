54cc721f9d4a6eae876ae2533d705f2beefd4378
rodrigodslima <rodrigodanidsl@gmail.com>
2025-10-26 20:56:46 -0300

    Projeto java bank na DIO

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("java")
+    id("application")
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+group = "br.com.dio"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    compileOnly("org.projectlombok:lombok")
+
+    annotationProcessor("org.projectlombok:lombok:1.18.36")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass.set("br.com.dio.Main")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "br.com.dio.Main"
+    }
+}
---
+++ src/main/java/br/com/dio/exception/NoFundsEnoughException.java
@@ -0,0 +1,9 @@
+package br.com.dio.exception;
+
+public class NoFundsEnoughException extends RuntimeException {
+
+    public NoFundsEnoughException(String message) {
+        super(message);
+    }
+
+}
